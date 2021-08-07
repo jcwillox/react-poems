7d22e569404f4b204d59ef36211ca28ff9a9811c
Josh Willox <joshwillox@gmail.com>
2021-08-07 18:27:43 +1000

    Add express backend

---
+++ src/server/server.ts
@@ -0,0 +1,22 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import cors from "cors";
+
+const app: Express = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static("build"));
+app.use(cors());
+
+/* basic logging handler */
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url);
+  next();
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile("build/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
+});
