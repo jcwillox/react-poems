6ddfd8b31a235b5a92156bd19084a2a1d03808f5
Josh Willox <joshwillox@gmail.com>
2021-08-11 15:55:50 +1000

    Add SearchAppBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { CssBaseline } from "@material-ui/core";
+import SearchAppBar from "./components/AppBar";
 
 function App() {
   return (
     <React.Fragment>
+      <SearchAppBar />
       <CssBaseline />
     </React.Fragment>
   );
