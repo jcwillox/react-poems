76c0b2e98cb5011e212fdb8f8f08939816ebb7da
Josh Willox <joshwillox@gmail.com>
2021-08-23 19:46:42 +1000

    Implement caching mechanism for PoemBackend

--- src/config/backend.ts
+++ src/config/backend.ts
@@ -3,14 +3,36 @@ import { PoemType } from "../models/Poem";
 let poemData: PoemType[] = [];
 
 export const PoemBackend = {
+  cache: {
+    /** returns a list of all poems from the cache */
+    all: (): PoemType[] => {
+      return poemData;
+    },
+    get: (id: string): PoemType | undefined => {
+      return poemData.find(poem => poem.id === id);
+    },
+    isValid: () => {
+      // right now we assume the cache is valid if it has any data
+      // however this function could in the future be used to expire
+      // the cache after some condition
+      return poemData.length > 0;
+    }
+  },
   /** returns a list of all poems */
   all: async (): Promise<PoemType[]> => {
+    // return from cache if populated
+    if (PoemBackend.cache.isValid()) {
+      return poemData;
+    }
     console.debug("fetching poems from backend");
     poemData = await PoemBackend.api("/poems");
     return poemData;
   },
   /** return a specific poem based on its id */
   get: async (id: string): Promise<PoemType | undefined> => {
+    if (PoemBackend.cache.isValid()) {
+      return poemData.find(poem => poem.id === id);
+    }
     console.debug("fetching poem from backend", id);
     return await PoemBackend.api(`/poems/${id}`);
   },
