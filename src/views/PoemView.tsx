85160772c4937bc1c503113932fe73e2bc7acdbe
Josh Willox <joshwillox@gmail.com>
2021-09-04 16:29:34 +1000

    Add Like and Share buttons to PoemView

--- src/views/PoemView.tsx
+++ src/views/PoemView.tsx
@@ -4,6 +4,8 @@ import { Box, Breadcrumbs, Link, Paper, Typography } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Markdown from "markdown-to-jsx";
 import { usePoem } from "../config/hooks";
+import LikesButton from "../components/LikesButton";
+import ShareButton from "../components/ShareButton";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,6 +15,14 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     title: {
       fontWeight: 300
+    },
+    container: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "flex-end"
+    },
+    separator: {
+      marginLeft: theme.spacing(1)
     }
   })
 );
@@ -24,15 +34,25 @@ const PoemView = () => {
   console.debug("reloading PoemView component", poem, id);
   return (
     <div>
-      <Breadcrumbs aria-label="breadcrumb">
-        <Link color="inherit" component={RouterLink} to="/">
-          Poems
-        </Link>
-        <Typography color="textPrimary">{poem?.author}</Typography>
-      </Breadcrumbs>
-      <Typography variant="h3" className={classes.title}>
-        {poem?.title}
-      </Typography>
+      <div className={classes.container}>
+        <div>
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link color="inherit" component={RouterLink} to="/">
+              Poems
+            </Link>
+            <Typography color="textPrimary">{poem?.author}</Typography>
+          </Breadcrumbs>
+          <Typography variant="h3" className={classes.title}>
+            {poem?.title}
+          </Typography>
+        </div>
+        <div>
+          <LikesButton poem={poem} />
+          <span className={classes.separator}>·</span>
+          <ShareButton id={poem?.id} />
+        </div>
+      </div>
+
       <Paper variant="outlined" className={classes.markdown}>
         <Box margin={2}>
           <Typography variant="body1" component="div">
