5b4a6d233799590db94e0c36a7d84f181c2720d2
Josh Willox <joshwillox@gmail.com>
2021-09-05 13:05:27 +1000

    Improve LikeButton error handling and add `notistack`

--- src/components/LikesButton.tsx
+++ src/components/LikesButton.tsx
@@ -6,6 +6,7 @@ import {
   Favorite as FavoriteIcon,
   FavoriteBorder as FavoriteBorderIcon
 } from "@material-ui/icons";
+import { useSnackbar } from "notistack";
 
 type LikesButtonProps = {
   poem?: PoemType;
@@ -13,21 +14,43 @@ type LikesButtonProps = {
 
 const LikesButton = ({ poem }: LikesButtonProps) => {
   const [liked, setLiked] = useState(poem?.liked);
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     setLiked(poem?.liked);
   }, [poem]);
 
-  const handleClick = () => {
-    if (!poem) return;
-    poem.liked = !liked;
-    setLiked(poem.liked);
-    if (poem.liked) {
-      PoemBackend.upvote(poem.id);
+  const changeLiked = (state: boolean) => {
+    if (poem === undefined) return;
+    poem.liked = state;
+    if (state) {
       poem.votes++;
     } else {
-      PoemBackend.downvote(poem.id);
       poem.votes--;
+    }
+    setLiked(state);
+  };
+
+  const handleClick = () => {
+    if (poem === undefined) return;
+    if (poem.liked) {
+      // optimistically set the liked state
+      changeLiked(false);
+      PoemBackend.downvote(poem.id).catch(err => {
+        // reset liked state if the request fails
+        changeLiked(true);
+        enqueueSnackbar("Error while downvoting poem: " + err, {
+          variant: "error"
+        });
+      });
+    } else {
+      changeLiked(true);
+      PoemBackend.upvote(poem.id).catch(err => {
+        changeLiked(false);
+        enqueueSnackbar("Error while upvoting poem: " + err, {
+          variant: "error"
+        });
+      });
     }
   };
 
@@ -36,7 +59,7 @@ const LikesButton = ({ poem }: LikesButtonProps) => {
       <IconButton aria-label="add to favorites" onClick={handleClick}>
         {liked ? <FavoriteIcon color="secondary" /> : <FavoriteBorderIcon />}
       </IconButton>
-      {poem?.votes || "?"}
+      {poem !== undefined ? poem.votes : "?"}
     </React.Fragment>
   );
 };
