a6d28640e3dc3152ed73395cdc5e306ee8f77dbd
Josh Willox <joshwillox@gmail.com>
2021-09-05 21:33:36 +1000

    Add additional poems
    Poems were sourced from https://www.public-domain-poetry.com

--- src/server/poems/data.ts
+++ src/server/poems/data.ts
@@ -8,7 +8,7 @@ export const defaultPoems: { [id: string]: PoemType } = {
     authorId: 0,
     text:
       "__Lorem__ ipsum dolor sit amet,  \nconsectetur adipiscing elit,  \n sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  \n Ut enim ad minim veniam,  \n quis nostrud exercitation  \n ullamco  \n laboris nisi ut aliquip ex ea commodo consequat.  \n Duis aute irure dolor  \n in reprehenderit in  \n voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n\n Excepteur sint occaecat cupidatat non proident,  \n sunt in culpa qui  \n officia deserunt mollit anim  \n id est  \n laborum.",
-    votes: 6
+    votes: 3
   },
   "1177f0b5-4d72-4387-a64a-6f0c3c307a07": {
     id: "1177f0b5-4d72-4387-a64a-6f0c3c307a07",
@@ -26,7 +26,7 @@ export const defaultPoems: { [id: string]: PoemType } = {
     authorId: 0,
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    votes: 7
+    votes: 2
   },
   "1bd396ae-681b-4e46-ae69-00609fbd04ec": {
     id: "1bd396ae-681b-4e46-ae69-00609fbd04ec",
@@ -36,5 +36,188 @@ export const defaultPoems: { [id: string]: PoemType } = {
     text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     votes: 2
+  },
+  "288888c7-010a-43f5-ab5d-2b05d3066c3c": {
+    id: "288888c7-010a-43f5-ab5d-2b05d3066c3c",
+    title: "Revelation",
+    author: "Robert Lee Frost",
+    authorId: 2,
+    votes: 8,
+    text:
+      "We make ourselves a place apart<br>\n" +
+      "Behind light words that tease and flout,<br>\n" +
+      "But oh, the agitated hear<br>\n" +
+      "Till someone really find us out.<br>\n" +
+      "<br>\n" +
+      "'Tis pity if the case require<br>\n" +
+      "(Or so we say) that in the end<br>\n" +
+      "We speak the literal to inspire<br>\n" +
+      "The understanding of a friend.<br>\n" +
+      "<br>\n" +
+      "But so with all, from babes that play<br>\n" +
+      "At hid-and-seek to God afar,<br>\n" +
+      "So all who hide too well away<br>\n" +
+      "Must speak and tell us where they are."
+  },
+  "95be8825-1b50-49ca-85d2-9091361664d0": {
+    id: "95be8825-1b50-49ca-85d2-9091361664d0",
+    title: 'To John Ruskin. (After Reading His "Modern Painters.")',
+    author: "Francis William Lauderdale Adams",
+    authorId: 3,
+    votes: 4,
+    text:
+      "Yes, you do well to mock us, you<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Who knew our bitter woe -<br>\n" +
+      "To jeer the false, deny the true<br>\n" +
+      "&nbsp;&nbsp;&nbsp;In us blind struggling low,<br>\n" +
+      "<br>\n" +
+      "While, on your pleasant place aloft<br>\n" +
+      "&nbsp;&nbsp;&nbsp;With flowers and clouds and streams,<br>\n" +
+      "At our black sweat and toil you scoffed<br>\n" +
+      "&nbsp;&nbsp;&nbsp;That marred your idle dreams.<br>\n" +
+      "<br>\n" +
+      '"_Oh_, _freedom_, _what was that to us_,"<br>\n' +
+      "(You'd shout down to us there),<br>\n" +
+      '"_Except the freedom foul_, _vicious_,<br>\n' +
+      "&nbsp;&nbsp;&nbsp;_From all of good and fair_?<br>\n" +
+      "<br>\n" +
+      '"_Obedience_, _faith_, _humility_,<br>\n' +
+      '&nbsp;&nbsp;&nbsp;_To us were empty names_." -<br>\n' +
+      "The like to you (might we reply)<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Whose noisy life proclaims<br>\n" +
+      "<br>\n" +
+      "Presumption, want of human love,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Impatience, filthy breath, {32}<br>\n" +
+      "The snob in soul who looks above,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Trampling on what's beneath.<br>\n" +
+      "<br>\n" +
+      "When did you strive, in nobler part,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;With love and gentleness,<br>\n" +
+      "To help one soul, to win one heart<br>\n" +
+      "&nbsp;&nbsp;&nbsp;To joy and hope and peace?<br>\n" +
+      "<br>\n" +
+      "Go to, vain prophet, without faith<br>\n" +
+      "&nbsp;&nbsp;&nbsp;In God who maketh new,<br>\n" +
+      "With hankerings for this putrid death,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;This Flesh-feast of the Few,<br>\n" +
+      "<br>\n" +
+      "This Social Structure of red mud,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;This Edifice of slime,<br>\n" +
+      "Whose bricks are bones, whose mortar's blood,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Whose pinnacle is Crime! -<br>\n" +
+      "<br>\n" +
+      "Go to, for we who strain our power<br>\n" +
+      "&nbsp;&nbsp;&nbsp;For light and warmth and scope,<br>\n" +
+      "For wives', for children's happier hour,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Can teach you faith and hope.<br>\n" +
+      "<br>\n" +
+      "Hark to the shout of those who cleared<br>\n" +
+      "&nbsp;&nbsp;&nbsp;The Missionary Ridge!<br>\n" +
+      "Look on those dead who never feared<br>\n" +
+      "&nbsp;&nbsp;&nbsp;The battle's bloody bridge!<br>\n" +
+      "<br>\n" +
+      "Watch the stern swarm at that last breach<br>\n" +
+      "&nbsp;&nbsp;&nbsp;March up that came not thence -<br>\n" +
+      "And learn Democracy can teach<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Divine obedience. {33}<br>\n" +
+      "<br>\n" +
+      "Pass through that South at last brought low<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Where loyal freemen live,<br>\n" +
+      "And learn Democracy knows how<br>\n" +
+      "&nbsp;&nbsp;&nbsp;To utterly forgive.<br>\n" +
+      "<br>\n" +
+      "Come then, and take this free-given bread<br>\n" +
+      "&nbsp;&nbsp;&nbsp;Of us who've scarce enough;<br>\n" +
+      "Hush your proud lips, bow down your head<br>\n" +
+      "&nbsp;&nbsp;&nbsp;And worship human love!<br>"
+  },
+  "a5c9d1f4-76ff-4384-a237-4497f0d7a020": {
+    id: "a5c9d1f4-76ff-4384-a237-4497f0d7a020",
+    title: "Prototypes",
+    author: "Madison Julius Cawein",
+    authorId: 4,
+    votes: 5,
+    text:
+      "Whether it be that we in letters trace<br>\n" +
+      "The pure exactness of a wood bird's strain,<br>\n" +
+      "And name it song; or with the brush attain<br>\n" +
+      "The high perfection of a wildflower's face;<br>\n" +
+      "Or mold in difficult marble all the grace<br>\n" +
+      "We know as man; or from the wind and rain<br>\n" +
+      "Catch elemental rapture of refrain<br>\n" +
+      "And mark in music to due time and place:<br>\n" +
+      "The aim of Art is Nature; to unfold<br>\n" +
+      "Her truth and beauty to the souls of men<br>\n" +
+      "In close suggestions; in whose forms is cast<br>\n" +
+      "Nothing so new but 'tis long eons old;<br>\n" +
+      "Nothing so old but 'tis as young as when<br>\n" +
+      "The mind conceived it in the ages past."
+  },
+  "719e9142-06b8-4640-8f42-68d4bba330b9": {
+    id: "719e9142-06b8-4640-8f42-68d4bba330b9",
+    title: "The Beleaguered City.",
+    author: "William Henry Giles Kingston",
+    authorId: 7,
+    votes: 7,
+    text:
+      "I have read, in some old marvellous tale,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;Some legend strange and vague,<br>\n" +
+      "That a midnight host of spectres pale<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;Beleaguered the walls of Prague.<br>\n" +
+      "<br>\n" +
+      "Beside the Moldau's rushing stream,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;With the wan moon overhead,<br>\n" +
+      "There stood, as in an awful dream,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;The army of the dead.<br>\n" +
+      "<br>\n" +
+      "White as a sea-fog, landward bound,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;The spectral camp was seen,<br>\n" +
+      "And with a sorrowful, deep sound,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;The river flowed between.<br>\n" +
+      "<br>\n" +
+      "No other voice nor sound was there,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;No drum, nor sentry's pace;<br>\n" +
+      "The mist-like banners clasped the air,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;As clouds with clouds embrace.<br>\n" +
+      "<br>\n" +
+      "But, when the old cathedral bell<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;Proclaimed the morning prayer,<br>\n" +
+      "The white pavilions rose and fell<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;On the alarmed air.<br>\n" +
+      "<br>\n" +
+      "Down the broad valley fast and far<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;The troubled army fled;<br>\n" +
+      "Up rose the glorious morning star,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;The ghastly host was dead.<br>\n" +
+      "<br>\n" +
+      "I have read in the marvellous heart of man,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;That strange and mystic scroll,<br>\n" +
+      "That an army of phantoms vast and wan<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;Beleaguer the human soul.<br>\n" +
+      "<br>\n" +
+      "Encamped beside Life's rushing stream,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;In Fancy's misty light,<br>\n" +
+      "Gigantic shapes and shadows gleam<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;Portentous through the night.<br>\n" +
+      "<br>\n" +
+      "Upon its midnight battle-ground<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;The spectral camp is seen,<br>\n" +
+      "And, with a sorrowful, deep sound,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;Flows the River of Life between.<br>\n" +
+      "<br>\n" +
+      "No other voice nor sound is there,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;In the army of the grave;<br>\n" +
+      "No other challenge breaks the air,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;But the rushing of Life's wave.<br>\n" +
+      "<br>\n" +
+      "And when the solemn and deep church bell<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;Entreats the soul to pray,<br>\n" +
+      "The midnight phantoms feel the spell,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;The shadows sweep away.<br>\n" +
+      "<br>\n" +
+      "Down the broad Vale of Tears afar<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;The spectral camp is fled;<br>\n" +
+      "Faith shineth as a morning star,<br>\n" +
+      "&nbsp;&nbsp;&nbsp;&nbsp;Our ghastly fears are dead."
   }
 };
